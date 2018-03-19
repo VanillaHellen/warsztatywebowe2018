95bbe3e2a15bfd869b15afcdc66114a95165824f
VanillaHellen <vanillahellen@gmail.com>
2018-03-19 19:37:50 +0100

    Piano Learning site - HTML base

---
+++ web000/zaj2 - piano/nauczone.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+
+<head>
+    <meta charset="utf8_encode"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Learning Piano</title>
+</head>
+
+<body>   
+<center>
+<div class="dbody">
+<div id="logo"><img src="http://via.placeholder.com/800x200"></div>
+
+<a href="index.php"><button class="button">dodaj</button></a>
+<a href="dodane.php"><button class="button">In-progress</button></a>
+<a href="nauczone.php"><button class="button buttonactive">Nauczone</button></a>
+
+<div id="newtask">
+
+~~~ SITE IN PROGRESS ~~~
+
+</div>
+
+</div>
+</body>
+
+</html>
